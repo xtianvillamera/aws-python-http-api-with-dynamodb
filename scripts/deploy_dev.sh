8cce81d0af6f11f920be7c7961d499d3bbdee0de
Christian Villamera <villamera.christian@gmail.com>
2023-02-19 21:11:32 +0800

    add deploy script

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,3 +1,11 @@
 serverless -v
 
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION='us-east-1'
+
+serverless config credentials --key AWS_ACCESS_KEY_ID --secret AWS_SECRET_ACCESS_KEY --overwrite
+
+serverless deploy
+
 echo "Done deployment"

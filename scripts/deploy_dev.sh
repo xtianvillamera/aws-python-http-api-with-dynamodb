6496d03e11a139e67115e437306016a65f3640df
Christian Villamera <villamera.christian@gmail.com>
2023-02-19 21:13:23 +0800

    add provider in deploy script

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -4,7 +4,7 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION='us-east-1'
 
-serverless config credentials --key AWS_ACCESS_KEY_ID --secret AWS_SECRET_ACCESS_KEY --overwrite
+serverless config credentials --provider aws --key AWS_ACCESS_KEY_ID --secret AWS_SECRET_ACCESS_KEY --overwrite
 
 serverless deploy
 

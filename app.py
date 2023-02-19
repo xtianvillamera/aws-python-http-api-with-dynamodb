666dcec430852ab170dc96983dfeca6a2cc2ae15
Christian Villamera <villamera.christian@gmail.com>
2023-02-19 22:08:21 +0800

    add linting

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 
+
 import boto3
 from flask import Flask, jsonify, make_response, request
 
@@ -9,10 +10,13 @@ dynamodb_client = boto3.client('dynamodb')
 
 if os.environ.get('IS_OFFLINE'):
     dynamodb_client = boto3.client(
-        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
+        'dynamodb', region_name='localhost',
+        endpoint_url='http://localhost:8000'
     )
 
+
 USERS_TABLE = os.environ['USERS_TABLE']
+
 
 @app.route('/users/<string:user_id>')
 def get_user(user_id):
@@ -21,25 +25,32 @@ def get_user(user_id):
     )
     item = result.get('Item')
     if not item:
-        return jsonify({'error': 'Could not find user with provided "userId'}), 404
+        return jsonify({'error': 'Could not find user with provided "userId'}),
+        404
 
     return jsonify(
-        {'userId': item.get('userId').get('S'),
-        'name': item.get('name').get('S')}
+        {
+            'userId': item.get('userId').get('S'),
+            'name': item.get('name').get('S')}
     )
+
 
 @app.route('/users', methods=['POST'])
 def create_user():
     user_id = request.json.get('userId')
     name = request.json.get('name')
     if not user_id or not name:
-        return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
+        return jsonify({'error': 'Please provide both "userId" and "name"'}),
+        400
 
     dynamodb_client.put_item(
-        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': name}}
+        TableName=USERS_TABLE, Item={
+            'userId': {'S': user_id},
+            'name': {'S': name}}
     )
 
     return jsonify({'userId': user_id, 'name': name})
+
 
 @app.errorhandler(404)
 def resource_not_found(e):
